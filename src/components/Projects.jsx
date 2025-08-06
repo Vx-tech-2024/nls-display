d2c6eef43457345505500955a96b0b19fd841bf5
Vx-tech-2024 <xaviervianney8@gmail.com>
2025-08-06 16:36:28 +0300

    projects slide update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
 
       <div className="w-full h-full flex flex-col items-center justify-center px-8">
         <h2 className="text-5xl font-bold mb-10 text-center text-gray-900">
-          📊 Last Month and This Month Projects
+          📊 Completed Projects Last Month and This Month 
         </h2>
 
         <table className="table-auto border-collapse w-full max-w-4xl text-2xl text-left shadow-xl">
